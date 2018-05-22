857f9bf79eef4a9f116b7cf0d4704d49f2de310a
Arturo <arturog278@gmail.com>
2018-05-21 19:14:01 -0500

    Initial Commit

---
+++ Proyecto3D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proyecto3D
+//
+//  Created by Arturo Garcia on 21/05/18.
+//  Copyright © 2018 Arturo Garcia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
